b58f19a4d01742e1085ff1cd1a827ad47dd8164f
Mark Stoer <49734282+mcstoer@users.noreply.github.com>
2023-07-31 15:23:10 -0700

    Add drawing of lines for the board.

--- src/game.cpp
+++ src/game.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
@@ -14,13 +13,38 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
+// Draw lines for game board
+void draw_board_lines() {
+    
+    int lines = 9;
+    float side_length = 2.0f - 0.2f;
+
+    float top = side_length / 2 - 0.1f;
+    float bottom = -top;
+
+    glBegin(GL_LINES);
+
+    // Try Vertical Lines First
+    for (int i = -4; i <= 4; i++) {
+        float horizontal_pos = side_length / lines  * i;
+        glVertex2f(horizontal_pos, top);
+        glVertex2f(horizontal_pos, bottom);
+
+        // Horz
+        glVertex2f(top, horizontal_pos);
+        glVertex2f(bottom, horizontal_pos);
+    }
+
+    glEnd();
+}
+
 int main(void)
 {
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(960, 720, "Simple example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -42,7 +66,9 @@ int main(void)
         glLoadIdentity();
         glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
+        
+        draw_board_lines();
+        /*
         glBegin(GL_TRIANGLES);
         glColor3f(1.f, 0.f, 0.f);
         glVertex3f(-0.6f, -0.4f, 0.f);
@@ -51,6 +77,13 @@ int main(void)
         glColor3f(0.f, 0.f, 1.f);
         glVertex3f(0.f, 0.6f, 0.f);
         glEnd();
+        glBegin(GL_LINES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-1.0f, -0.4f, 0.f);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-1.1f, -0.4f, 0.f);
+        glEnd();
+        */
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
