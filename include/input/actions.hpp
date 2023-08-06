0e1e920fc8934c279b2925cad70e2fbb8a3871ec
Mark Stoer <49734282+mcstoer@users.noreply.github.com>
2023-08-06 11:48:45 -0700

    Fix building to use new main file and add action header for relaying player actions.

---
+++ include/input/actions.hpp
@@ -0,0 +1,14 @@
+#ifndef actions_hpp
+#define actions_hpp
+
+// Defines possible actions taken by a player
+enum class Action {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+    ROTATE,
+    PLACE
+};
+
+#endif 
