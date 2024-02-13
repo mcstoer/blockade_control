ffd4418b44754e13e360f26019caef98c13e66bf
Mark Stoer <49734282+mcstoer@users.noreply.github.com>
2024-02-13 11:21:08 -0800

    Add comments to board test cases to improve code readability.

--- tests/test_board.cpp
+++ tests/test_board.cpp
@@ -7,7 +7,8 @@
 #include "components/piece.hpp"
 #include "components/rectangle.hpp"
 
-
+// Tests the placement of valid and invalid placements. Also checks if
+// placing pieces on the board does not modify the points.
 TEST_CASE("place", "[place]") {
 
     Board board; 
@@ -19,19 +20,27 @@ TEST_CASE("place", "[place]") {
     int x = 5;
     int y = 1;
 
+    // First placement on a empty board is valid.
     REQUIRE(board.place_piece(p, x, y));
     p2->rotate();
     p2->rotate();
+
+    // Second placement by different owner in the same square but
+    // 180 degrees rotated is valid.
     REQUIRE(board.place_piece(p2, x, y));
+
+    // Third placement on a filled square should be invalid.
     REQUIRE_FALSE(board.place_piece(p3, x, y));
 
 
     auto ptr = board.get_board();
 
+    // The points should not get modified by placements.
     REQUIRE(ptr[y][x].first->get_points() == p->get_points());
     REQUIRE(ptr[y][x].second->get_points() == p2->get_points());
 }
 
+// Tests clearing a board of all pieces.
 TEST_CASE("clear", "[clear]") {
 
     Board board; 
